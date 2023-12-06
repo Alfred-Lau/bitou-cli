7fc5036c8c9c004f38f012f1c0d3ad03417211be
Alfred-Lau <jian.liu@icloud.com>
2023-12-06 11:46:02 +0800

    feat: add readme

--- src/plugins/seo/aftercheck.ts
+++ src/plugins/seo/aftercheck.ts
@@ -5,6 +5,7 @@ export default function afterCheck(params) {
   // 1. 检查给定url的页面的爬虫视角
   if (typeof params === 'string') {
     // curl https://www.oceanbase.com/demo  -H 'User-Agent: baiduspider'
-    shell.exec(`curl ${params} -H 'User-Agent: baiduspider'`)
+    const content = shell.exec(`curl ${params} -H 'User-Agent: baiduspider'`)
+    console.log('content', content.stdout)
   }
 }
