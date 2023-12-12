0618083188976670f745987aa08a6ac7a7920826
Alfred-Lau <jian.liu@icloud.com>
2023-12-12 15:01:37 +0800

    feat: add compiler

--- src/plugins/seo/ast/compiler.ts
+++ src/plugins/seo/ast/compiler.ts
@@ -1,6 +1,14 @@
 import compiler from 'typescript'
 class AstCompiler {
 
+  public scanFiles() {
+    // scan files and return array of file paths
+
+  }
+  public compile(code: string) {
+    return compiler.transpile(code);
+  }
+
 }
 
 export default AstCompiler;
