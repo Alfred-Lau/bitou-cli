f0b1c0fb81791f473ef162fa60d492dc8f02b109
Alfred-Lau <jian.liu@icloud.com>
2023-12-15 00:59:36 +0800

    feat: process.exit

--- src/plugins/performance/index.ts
+++ src/plugins/performance/index.ts
@@ -1,17 +1,18 @@
-import calculateLHR from "./lhr";
-import showTable from "./show";
+import calculateLHR from './lhr';
+import showTable from './show';
 
 export default async (name, options, command) => {
-
   if (options.debug) {
     console.error('Called %s with options %o', command.name(), options);
   }
 
   let collected = [
-    ["page url", "perf score", "lcp", "fid", "cls", "ttfb"]
-  ] as any
+    ['page url', 'perf score', 'lcp', 'fid', 'cls', 'ttfb'],
+  ] as any;
 
-  const item = await calculateLHR(name)
-  collected.push(item)
-  showTable(collected)
-}
+  const item = await calculateLHR(name);
+  collected.push(item);
+  showTable(collected);
+  // 结束之后退出进程
+  process.exit(0);
+};
